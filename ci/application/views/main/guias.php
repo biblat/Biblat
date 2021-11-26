953f0e8ae8e4a6d9e4825295721dc5e83cea5143
Edgar <eduranm@dgb.unam.mx>
2021-11-25 23:21:20 -0600

    Sección Guías

---
+++ ci/application/views/main/guias.php
@@ -0,0 +1,13 @@
+<div class="panel-group">
+        <div class="panel panel-default">
+		<div class="panel-heading">
+			<h5 class="panel-title">
+				<a href="{base_url('archivos/pdf/InstruccionesIncrustarIndicadores.pdf')}" target="_blank">
+                                    <span class="fa fa-file-pdf-o"></span> Cómo incrustar indicadores en el portal de tu revista.
+				</a>
+			</h5>
+		</div>
+	</div>
+	
+</div>
+<br/>
