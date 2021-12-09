813c06a824222bf0cb0013d56b24cde7a7132b2f
Edgar <eduranm@dgb.unam.mx>
2021-12-09 00:03:39 -0600

    Dashboard Macrouniversidades

---
+++ ci/application/views/main/dashboardmu.php
@@ -0,0 +1,6 @@
+
+<div class="container-iframe">
+    <iframe class="responsive-iframe" 
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRVpGwwZhmkypZBPSSJXBboe4JmhiA5wjLM2Q7O_x68nGAFB0-_REvRBXimD74S1485245WinAXMGt0/pubhtml?widget=true&amp;headers=false"
+    ></iframe>
+</div>
