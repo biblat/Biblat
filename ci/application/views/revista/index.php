1de64f9719a7b18d8fa92676c9941b22957c7796
Edgar <eduranm@dgb.unam.mx>
2022-05-09 15:38:04 -0500

    Agrega insignias

--- ci/application/views/revista/index.php
+++ ci/application/views/revista/index.php
@@ -12,29 +12,35 @@
         <tr>
           <td>{$key}.-</td>
           <td>
-            <span class="article-tittle">{$resultado.articuloLink}</span><br/>
-{if $resultado.autoresHTML}
-           {$resultado.autoresHTML}<br/>
-{/if}
-{if $resultado.institucionesHTML}
-           {$resultado.institucionesHTML}<br/>
-{/if}
-{if $resultado.detalleRevista}
-           {$resultado.detalleRevista}<br/>
-{/if}
-{if !$resultado.addRef}
-           {$resultado.addRef}<br/>
-{/if}
+                <div class="row">
+                <div class="col-xs-12 col-md-9">
+                    <span class="article-tittle">{$resultado.articuloLink}</span><br/>
+                        {if $resultado.autoresHTML}
+                                   {$resultado.autoresHTML}<br/>
+                        {/if}
+                        {if $resultado.institucionesHTML}
+                                   {$resultado.institucionesHTML}<br/>
+                        {/if}
+                        {if $resultado.detalleRevista}
+                                   {$resultado.detalleRevista}<br/>
+                        {/if}
+                        {if !$resultado.addRef}
+                                   {$resultado.addRef}<br/>
+                        {/if}
+                        <br>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                    <div style="display: flex; align-items: center; justify-content: center">
+                        <div style="display: inline;" class='altmetric-embed'  data-badge-type='donut' data-link-target='_blank' data-doi='{$resultado.doi}' data-badge-popover='bottom' title="Altmetric"></div>
+                        <div style="display: inline;">&nbsp;&nbsp;&nbsp;</div>
+                        <div style="display: inline;"><span data-style="small_circle" class="__dimensions_badge_embed__" data-doi="{$resultado.doi}" data-legend="hover-bottom" title="Dimensions"></span></div>
+                        <div style="display: inline;"><a href="https://plu.mx/plum/a/?doi={$resultado.doi}" class="plumx-plum-print-popup" data-size="medium" data-popup="bottom" data-hide-when-empty="true" title="PlumX"></a></div>
+                    </div>
+                </div>
+                </div>
           </td>
           <td class="nowrap text-right">
-            {if $resultado.downloadLink}{$resultado.downloadLink}{/if} {$resultado.mendeleyLink}
-			<br/><br/>
-            <div style="display: flex">
-                <div style="display: inline;" class='altmetric-embed'  data-badge-type='donut' data-link-target='_blank' data-doi='{$resultado.doi}' data-badge-popover='left' title="Altmetric"></div>
-                <div style="display: inline;">&nbsp;&nbsp;&nbsp;</div>
-                <div style="display: inline;"><span data-style="small_circle" class="__dimensions_badge_embed__" data-doi="{$resultado.doi}" data-legend="hover-bottom" title="Dimensions"></span></div>
-                <div style="display: inline;"><a href="https://plu.mx/plum/a/?doi={$resultado.doi}" class="plumx-plum-print-popup" data-size="medium" data-popup="right" data-hide-when-empty="true" title="PlumX"></a></div>
-            </div>
+              {if $resultado.downloadLink}{$resultado.downloadLink}{/if} {$resultado.mendeleyLink}
           </td>
                 </tr>
 {/foreach}
