1b7b2c4a0bc94060a018a8670906e496d6748fa4
Edgar <eduranm@dgb.unam.mx>
2022-06-22 16:36:50 -0500

    Actualización bibmacro

--- ci/application/views/bibmacro/bibmacro.php
+++ ci/application/views/bibmacro/bibmacro.php
@@ -8,7 +8,7 @@ type="text/html">
     <div class="row">
         <div class="col-sm-8" id="mapa-biblat" class="chart_data"></div>
         <div class="col-sm-4" style="" id="div_tabla">
-            <center><br><br><div class="fa-2x" style="color:#ff8000"><i class="fa fa-refresh fa-spin"></i></div></center>
+            <center><br><br><div class="fa-2x" style="color:#00299e"><i class="fa fa-refresh fa-spin"></i></div></center>
         </div>
     </div>
 
@@ -20,7 +20,12 @@ type="text/html">
 
         </div>
     </div>
-
+<div class="row" id="div_revistas" hidden><div class="col-sm-12">
+            <br>
+            <center>
+                <b><span id="txt_revistas">Revistas</span></b><br>
+                <a id="reset_revistas" style="font-size:11px;cursor:pointer">Ver todas</a>
+            </center></div></div>
     <div class="row">
         <div class="col-sm-12" id="div_tbl_revistas">
 
