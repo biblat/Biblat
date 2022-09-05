d2103620b8c8863e5a00c4c37e8dd866c6dd11e9
Edgar <eduranm@dgb.unam.mx>
2022-09-05 15:49:05 -0500

    Actualización en preevaluación

--- ci/application/controllers/main.php
+++ ci/application/controllers/main.php
@@ -395,8 +395,8 @@ class Main extends CI_Controller{
             if(filter_var($_POST['completo'], FILTER_VALIDATE_BOOLEAN)){
                 $mensaje = "Estimado(a) editor(a):<br><br>".
                 "De acuerdo con la autoevaluación que realizó el día de hoy en nuestro sitio web biblat.unam.mx, le informamos que su revista cumple con los criterios obligatorios para ingresar a CLASE, PERIÓDICA, portal BIBLAT y catálogo SERIUNAM.<br><br>".
-                "Adjunto a este mensaje, usted encontrará su carta de postulación y un archivo con los resultados de su postulación. En este último, le hacemos llegar las plantillas donde deberá vaciar los metadatos de los artículos publicados en los 3 fascículos más recientes de su revista. ".
-                "Una vez completados, favor de enviarlos a esta misma dirección de correo electrónico.<br><br>".
+                "Adjunto a este mensaje, usted encontrará su carta de postulación y un archivo con los resultados de su postulación. En este último, encontrará un espacio para ingresar los datos editoriales de su revista. ".
+                "Una vez completados, favor de enviarlos a esta misma dirección de correo electrónico con copia a baguilar@dgb.unam.mx <br><br>".
 				"Le recordamos que su autoevaluación será analizada por nuestro Comité de Evaluación, y que este, podrá emitir recomendaciones previa indización de su revista.".
                 "Quedamos atentos a sus comentarios y agradecemos su colaboración.<br><br>".
                 "Saludos.<br>".
@@ -447,10 +447,10 @@ class Main extends CI_Controller{
                     );
             
             if(filter_var($_POST['completo'], FILTER_VALIDATE_BOOLEAN)){
-                $result = $this->setDocumentsDrive("CartaDePostulacion_".$_POST['issn'].".docx", 'Preevaluacion_'.$_POST['issn'].'.xlsx');
+                //$result = $this->setDocumentsDrive("CartaDePostulacion_".$_POST['issn'].".docx", 'Preevaluacion_'.$_POST['issn'].'.xlsx');
                 unlink("CartaDePostulacion_".$_POST['issn'].".docx");
                 unlink('Preevaluacion_'.$_POST['issn'].'.xlsx');
-                echo('{"docx":"'.$result[0].'", "xlsx":"'.$result[1].'"}');
+                //echo('{"docx":"'.$result[0].'", "xlsx":"'.$result[1].'"}');
             }
             else
                 unlink('Preevaluacion_'.$_POST['correo'].'.xlsx');
