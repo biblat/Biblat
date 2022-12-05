59f363e1a272c06e24f83c7b55e38e1a003448a5
Edgar <eduranm@dgb.unam.mx>
2022-12-05 08:51:22 -0600

    Manual de buenas prácticas

--- ci/application/views/main/guias.php
+++ ci/application/views/main/guias.php
@@ -1,5 +1,14 @@
 <div class="panel-group">
-        <div class="panel panel-default">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h5 class="panel-title">
+				<a href="{base_url('archivos/pdf/ManualDeIndizacionEnOJS_BuenasPracticas.pdf')}" target="_blank">
+                                    <span class="fa fa-file-pdf-o"></span> Manual de indización en OJS: Buenas prácticas para la región latinoamericana
+				</a>
+			</h5>
+		</div>
+	</div>
+    <div class="panel panel-default">
 		<div class="panel-heading">
 			<h5 class="panel-title">
 				<a href="{base_url('archivos/pdf/InstruccionesIncrustarIndicadores.pdf')}" target="_blank">
