fa83d0ea93a52ba1da89e1f250dbcd7293ae2dcc
Edgar <eduranm@dgb.unam.mx>
2023-11-23 10:36:01 -0600

    Modulos para análisis

---
+++ ci/application/views/admin/asigna.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div id="div_tabla" style="display:block">
+        </div>
+    </div>
+</div>
+<div class="row"><br></div>
+<div class="row"><br></div>
+<div class="row">
+    <div class="col-sm-12">
+        <div id="respBiblat"></div>
+        <br><br>
+        <div id="respOJS"></div>
+        <br><br>
+        <center><div id="mensajeFin"></div></center>
+    </div>
+</div>
+<div class="row"><br></div>
+
---
+++ ci/application/views/admin/avance.php
@@ -0,0 +1,15 @@
+<div class="row"><br></div>
+<center><div class="row"><b>Meta del departamento:</b> 10000 Registros</div></center>
+<div class="row"><br></div>
+<div class="row"><br></div>
+<div class="progress">
+  
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div id="div_tabla" style="display:block">
+        </div>
+    </div>
+</div>
+
+<div class="row"><br></div>
