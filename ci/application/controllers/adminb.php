ffacb44266a0505d8ea7dac8b5ebc933487082ec
Edgar <eduranm@dgb.unam.mx>
2023-11-23 09:07:57 -0600

    Módulo Administración

--- ci/application/controllers/adminb.php
+++ ci/application/controllers/adminb.php
@@ -5,33 +5,33 @@ class Adminb extends CI_Controller {
     
     public function __construct(){
             parent::__construct();
-			if (in_array($_SERVER['REMOTE_ADDR'], IPS)){											
-				$this->output->enable_profiler($this->config->item('enable_profiler'));
-				$this->template->set_partial('biblat_js', 'javascript/biblat', array(), TRUE, FALSE);
-				$this->template->set_partial('submenu', 'layouts/submenu');
-				$this->template->set_partial('search', 'layouts/search');
-				$this->template->set_breadcrumb(_('Inicio'), site_url('/'));
-				$this->template->set('class_method', $this->router->fetch_class().$this->router->fetch_method());
-				
-				$this->template->js('assets/js/highcharts/phantomjs/highcharts8.js');
-				$this->template->js('assets/js/apigoogle/api.js');
-				$this->template->js('assets/js/apigoogle/getaccesstokenfromserviceaccount.js');
-				$this->template->js('assets/js/apigoogle/client.js');
-				$this->template->css('css/jquery.slider.min.css');
-				$this->template->js('js/env.js');
-				$this->template->css('css/colorbox.css');
-				$this->template->js('js/jquery.slider.min.js');
-				$this->template->js('js/jquery.serializeJSON.min.js');
-				$this->template->js('js/colorbox.js');
-				$this->template->js('assets/js/utils/utils.js');
-				$this->template->js('assets/js/datatables/datatables.min.js');
+            if (in_array($this->get_ip(), IPS)){
+                $this->output->enable_profiler($this->config->item('enable_profiler'));
+                $this->template->set_partial('biblat_js', 'javascript/biblat', array(), TRUE, FALSE);
+                $this->template->set_partial('submenu', 'layouts/submenu');
+                $this->template->set_partial('search', 'layouts/search');
+                $this->template->set_breadcrumb(_('Inicio'), site_url('/'));
+                $this->template->set('class_method', $this->router->fetch_class().$this->router->fetch_method());
+
+                $this->template->js('assets/js/highcharts/phantomjs/highcharts8.js');
+                $this->template->js('assets/js/apigoogle/api.js');
+                $this->template->js('assets/js/apigoogle/getaccesstokenfromserviceaccount.js');
+                $this->template->js('assets/js/apigoogle/client.js');
+                $this->template->css('css/jquery.slider.min.css');
+                $this->template->js('js/env.js');
+                $this->template->css('css/colorbox.css');
+                $this->template->js('js/jquery.slider.min.js');
+                $this->template->js('js/jquery.serializeJSON.min.js');
+                $this->template->js('js/colorbox.js');
+                $this->template->js('assets/js/utils/utils.js');
+                $this->template->js('assets/js/datatables/datatables.min.js');
                 $this->template->js('assets/js/datatables/input.js');
             }else{
                 redirect('main');
-			}															  
+            }
     }
     
-    public function index(){
+    public function index(){   
         if($this->session->userdata('usuario')){
             $this->template->set_breadcrumb(_('Menú ').$this->session->userdata('rol'));
             $data = array();
@@ -64,7 +64,7 @@ class Adminb extends CI_Controller {
         }
     }
     
-	public function asigna(){
+    public function asigna(){
         if($this->session->userdata('usuario')){
             $this->template->set_breadcrumb(_('Menú Administrador'), site_url('adminb'));
             $this->template->set_breadcrumb(_('Asignación'));
@@ -113,5 +113,31 @@ class Adminb extends CI_Controller {
         }else{
             redirect('main');
         }
-    }						 
+    }
+    
+    private function get_ip(){
+        $realip = '';
+        if ($_SERVER) {  
+		   if ( $_SERVER["HTTP_X_FORWARDED_FOR"] ) {  
+			   $realip = $_SERVER["HTTP_X_FORWARDED_FOR"];  
+		   } elseif ( $_SERVER["HTTP_CLIENT_IP"] ) {  
+			   $realip = $_SERVER["HTTP_CLIENT_IP"];  
+		   } elseif ($_SERVER["REMOTE_ADDR"]){
+			   $realip = $_SERVER["REMOTE_ADDR"];  
+		   } else{
+			   $realip = 'UNKNOWN';
+		   }
+		} else {  
+			if ( getenv( 'HTTP_X_FORWARDED_FOR' ) ) {  
+			   $realip = getenv( 'HTTP_X_FORWARDED_FOR' );  
+			} elseif ( getenv( 'HTTP_CLIENT_IP' ) ) {  
+			   $realip = getenv( 'HTTP_CLIENT_IP' );  
+			} elseif (getenv( 'REMOTE_ADDR' )){  
+			   $realip = getenv( 'REMOTE_ADDR' );  
+			} else{
+			   $realip = 'UNKNOWN';
+			}
+		}
+		return $realip;
+	}
 }
