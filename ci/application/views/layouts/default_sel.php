897300a1cbf17b941708d2125881a6985af56e01
Edgar <eduranm@dgb.unam.mx>
2024-02-21 15:52:07 -0600

    Corrige error al dar hacia atrás

--- ci/application/views/layouts/default_sel.php
+++ ci/application/views/layouts/default_sel.php
@@ -331,5 +331,13 @@
 			{$template.partials.main_js}
 {/if}
 </script>
+
+<script>
+    window.addEventListener('pageshow', (event) => {
+        if (event.persisted) {
+            window.location.reload();
+        } 
+    });
+</script> 
     </body>
 </html>
