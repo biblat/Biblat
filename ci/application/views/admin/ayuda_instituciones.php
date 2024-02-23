9519879a94f5a3629a70309ac4daa75af376db80
Edgar <eduranm@dgb.unam.mx>
2024-02-23 15:35:26 -0600

    Actualización en Ayuda

--- ci/application/views/admin/ayuda_instituciones.php
+++ ci/application/views/admin/ayuda_instituciones.php
@@ -1,15 +1,15 @@
 <div class="row" style="padding:50px;">
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/16GUWsuJAGw19es4lDkx8-GOwjiRNyg-J/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Instituciones</span></a>
+        <a href="https://drive.google.com/file/d/1wr0JqmJGQRgQIfLD3_Iuv55vtWiaqcBs/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Instituciones</span></a>
     </div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1QZAknUAT90jL4I_A0mNFTZfK2plTngw9/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style=";color:black;">Validación</span></a>
+        <a href="https://drive.google.com/file/d/1QamUmlQdl4umocT0MVspcoo3nZr2Km/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style=";color:black;">Validación</span></a>
     </div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1NmPUx9aovsEUDZ-rvLL2SPaPE7ufRFI1/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">País</span></a>
+        <a href="https://drive.google.com/file/d/1ncXagYjqf64D3bioY-i8D7g7JfDSzwW3/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">País</span></a>
     </div>
     
     <div class="col-xs-12" style="padding:50px;height:480px;width: 100%;">
-        <iframe id="frame-video" src="https://drive.google.com/file/d/16GUWsuJAGw19es4lDkx8-GOwjiRNyg-J/preview" style="width:100%;height:100%;" allow="autoplay"></iframe>
+        <iframe id="frame-video" src="https://drive.google.com/file/d/1wr0JqmJGQRgQIfLD3_Iuv55vtWiaqcBs/preview" style="width:100%;height:100%;" allow="autoplay"></iframe>
     </div>
 </div>
