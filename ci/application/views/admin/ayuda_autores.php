3ec1d272dc680fac0b3f318e3fd64aae6ef8f05e
Edgar <eduranm@dgb.unam.mx>
2024-02-26 09:11:14 -0600

    Cambios en ayuda

--- ci/application/views/admin/ayuda_autores.php
+++ ci/application/views/admin/ayuda_autores.php
@@ -1,11 +1,12 @@
 <div class="row" style="padding:50px;">
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1bimAQQ3qH4w9wnVloHYznSMLlKuRvsPl/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Autores</span></a>
+        <a href="https://drive.google.com/file/d/1bimAQQ3qH4w9wnVloHYznSMLlKuRvsPl/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Bloque 2. Autores</span></a>
     </div>
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1MINF7LGkw1KJ1BjpQXxi6KU58AW7WKwL/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style=";color:black;">Metadatos de autores</span></a>
+        <a href="https://drive.google.com/file/d/1MINF7LGkw1KJ1BjpQXxi6KU58AW7WKwL/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style=";color:black;">Metadatos bloque autores</span></a>
     </div>
-    
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12" style="padding:50px;height:480px;width: 100%;">
         <iframe id="frame-video" src="https://drive.google.com/file/d/1Ou0rfwbMYfFl5o-2AKYbo6mtwHPUN8EU/preview" style="width:100%;height:100%;" allow="autoplay"></iframe>
     </div>
--- ci/application/views/admin/ayuda_general.php
+++ ci/application/views/admin/ayuda_general.php
@@ -1,20 +1,24 @@
 <div class="row" style="padding:50px;">
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1L3GEnctnAre4ThmzfXjyCeue_-7IrSEx/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Ingreso</span></a>
+        <a href="https://drive.google.com/file/d/1L3GEnctnAre4ThmzfXjyCeue_-7IrSEx/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Ingreso a Biblat Central</span></a>
     </div>
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1PI_BD4JJm9f0IwKivgphbIO25YkdRHSR/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style=";color:black;">Módulos</span></a>
+        <a href="https://drive.google.com/file/d/1PI_BD4JJm9f0IwKivgphbIO25YkdRHSR/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style=";color:black;">Módulos de Biblat Central</span></a>
     </div>
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1gg0IzInHaxJjRn3bfnV_8q9QpO53L9cB/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Documentos</span></a>
+        <a href="https://drive.google.com/file/d/1gg0IzInHaxJjRn3bfnV_8q9QpO53L9cB/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Selección de documentos</span></a>
     </div>
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1jfshoqk4JcnoMHX9p4oGd8Fpi5d8uYpc/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Análisis de registros</span></a>
+        <a href="https://drive.google.com/file/d/1jfshoqk4JcnoMHX9p4oGd8Fpi5d8uYpc/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Introducción a Bloques</span></a>
     </div>
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12">
-        <a href="https://drive.google.com/file/d/1fxtB44p8xnnQy3YKNUayNFGZuvdA-frV/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">Indizar un registro</span></a>
+        <a href="https://drive.google.com/file/d/1fxtB44p8xnnQy3YKNUayNFGZuvdA-frV/preview" class="video"><i class="fa fa-file-video-o" style="font-size: 20px;"></i> <span style="color:black;">¿Cómo realizar un registro?</span></a>
     </div>
-    
+    <div class="col-xs-12"><br></div>
     <div class="col-xs-12" style="padding:50px;height:480px;width: 100%;">
         <iframe id="frame-video" src="https://drive.google.com/file/d/1L3GEnctnAre4ThmzfXjyCeue_-7IrSEx/preview" style="width:100%;height:100%;" allow="autoplay"></iframe>
     </div>
