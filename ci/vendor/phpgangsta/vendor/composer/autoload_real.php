d9ba04331bc9fa2805e708def63e2fbb984fc80e
Edgar <eduranm@dgb.unam.mx>
2024-04-12 11:05:44 -0600

    Bib Google Authenticator

--- ci/vendor/phpgangsta/vendor/composer/autoload_real.php
+++ ci/vendor/phpgangsta/vendor/composer/autoload_real.php
@@ -27,9 +27,27 @@ class ComposerAutoloaderInit5988514fd4a6ea2f50c43b6df7a15df1
         spl_autoload_register(array('ComposerAutoloaderInit5988514fd4a6ea2f50c43b6df7a15df1', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5988514fd4a6ea2f50c43b6df7a15df1', 'loadClassLoader'));
-
-        require __DIR__ . '/autoload_static.php';
-        \Composer\Autoload\ComposerStaticInit5988514fd4a6ea2f50c43b6df7a15df1::getInitializer($loader)();
+        
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+            \Composer\Autoload\ComposerStaticInit5988514fd4a6ea2f50c43b6df7a15df1::getInitializer($loader)();
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+            
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+            
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
