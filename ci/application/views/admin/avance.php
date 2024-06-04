8dbf62239a8549b3d5705ab22183e359fe2ed000
Edgar <eduranm@dgb.unam.mx>
2024-06-04 14:59:35 -0600

    Avance por mes

--- ci/application/views/admin/avance.php
+++ ci/application/views/admin/avance.php
@@ -1,3 +1,18 @@
+<style>
+    #avance-actual{
+        transition: font-size 0.3s ease; 
+    }
+    .avance-mes{
+        transition: font-size 0.3s ease;
+    }
+    #avance-actual:hover{
+        font-size: 15px;
+    }
+    .avance-mes:hover{
+        font-size: 15px;
+    }
+</style>
+
 <script>
     const cons =    { 
                         rol: Object.freeze( { val: '<?php echo $rol; ?>'})
@@ -10,13 +25,22 @@
 <div class="progress esperado">
   
 </div>
-<div class="row"><div class="col-sm-12"><center><b>Avance:</b> <span id='avance_total'></span> Registros</center></div></div>
+<div class="row"><div class="col-sm-12"><center id="avance-actual"><b>Avance:</b> <span id='avance_total'></span> Registros</center></div></div>
 <div class="progress" style='height: 40px'>
   
 </div>
 <div class="row">
     <div class="col-sm-12">
         <div id="div_tabla" style="display:block">
+        </div>
+    </div>
+</div>
+
+<div class="row"><br><br></div>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div id="div_tiempos" style="display:block">
         </div>
     </div>
 </div>
