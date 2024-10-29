d32c86482139c2cc85c954b7a1d020904941bb55
eduranm <eduranm@dgb.unam.mx>
2024-10-29 14:41:06 -0600

    Actualización de textos

--- ci/application/views/verificador/info_metametrics.php
+++ ci/application/views/verificador/info_metametrics.php
@@ -1,5 +1,4 @@
 <p>MetaMetrics valida los metadatos a nivel artículo de las revistas publicadas en Open Journal System (OJS). Esta aplicación se basa en el <a href="{base_url('archivos/pdf/manual_indizacion_ojs_2ed.pdf')}" target="_blank">Manual de indización en OJS: Buenas prácticas para la región latinoamericana</a>, el cual constituye un marco de trabajo común para los países de América Latina y el Caribe.</p>
-<p>Para utilizar MetaMetrics es necesario instalar el plugin de BIBLAT para la recolección de metadatos en el OJS de la revista, las instrucciones están disponibles en <a href="{base_url('archivos/pdf/PluginBIBLAT.pdf')}" target="_blank">este enlace</a>.
     <br><br>
 La validación que realiza MetaMetrics comprende los siguientes aspectos:
 </p>
