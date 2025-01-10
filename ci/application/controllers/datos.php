90b178883877b97f47f481e7a82d82d0cbbf3aa1
eduranm <eduranm@dgb.unam.mx>
2025-01-10 09:34:23 -0600

    Actualización de consultas

--- ci/application/controllers/datos.php
+++ ci/application/controllers/datos.php
@@ -11,6 +11,12 @@ class Datos extends REST_Controller {
 	public function datosPais_get(){
 		$data = array();
 		$this->load->database();
+                
+                $prequery ="SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'claper' AND pid <> pg_backend_pid() AND 
+                  (state = 'idle' OR now() - backend_start > interval '10 minutes')";
+                
+                $this->db->query($prequery);
+                
 		$query = "select \"paisRevistaSlug\" pais, count(distinct slug(\"revista\")) revistas, sum(art) articulos from(" .
                             "select ve.\"paisRevistaSlug\", ve.\"revista\", count(1) art from \"mvPaisRevistaArticulo\" ve group by 1,2".
                             ") a group by \"paisRevistaSlug\"";
