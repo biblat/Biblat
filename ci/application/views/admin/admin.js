9f56c430a88e2ed143882cb1ff16c3f8b24703a9
eduranm <eduranm@dgb.unam.mx>
2025-02-20 15:00:57 -0600

    Importar XML

--- ci/application/views/admin/admin.js
+++ ci/application/views/admin/admin.js
@@ -294,30 +294,48 @@ class_admin = {
             class_admin.var.revista = class_utils.find_prop(class_admin.var.revistasJSON, 0, $('#select2-revista_sel-container').text());
             
             event.preventDefault();  // Prevenir el comportamiento por defecto del formulario
+            
+            $.confirm({
+                title: '',
+                content: 'Se ingresarán a Biblat los documentos encontrados en el archivo seleccionado',
+                buttons: {
+                    cancelar: {
+                            text: 'Cancelar',
+                            //btnClass: 'btn-red',
+                            action: function(){
+                            }
+                    },
+                    aceptar: {
+                            text: 'Aceptar',
+                            btnClass: 'btn-warning',
+                            action: function(){
+                               var formData = new FormData();
+                                formData.append("file", $("#archivo")[0].files[0]);
+                                formData.append("revista", class_admin.var.revista[0].trim());
+                                formData.append("base", class_admin.var.revista[1].trim());
+                                formData.append("pais", class_admin.var.revista[6].trim());
+                                formData.append("issn", class_admin.var.revista[5].trim());
+                                formData.append("institucion_editora", class_admin.var.revista[8].trim());
+                                formData.append("ciudad_editora", class_admin.var.revista[7].trim());
+                                formData.append("disciplina", class_admin.var.revista[2].trim());
 
-            var formData = new FormData();
-            formData.append("file", $("#archivo")[0].files[0]);
-            formData.append("revista", class_admin.var.revista[0].trim());
-            formData.append("base", class_admin.var.revista[1].trim());
-            formData.append("pais", class_admin.var.revista[6].trim());
-            formData.append("issn", class_admin.var.revista[5].trim());
-            formData.append("institucion_editora", class_admin.var.revista[8].trim());
-            formData.append("ciudad_editora", class_admin.var.revista[7].trim());
-            formData.append("disciplina", class_admin.var.revista[2].trim());
-
-            // Enviar el archivo a la aplicación Python
-            $.ajax({
-                url: class_admin.cons.send_xml,  // URL de la aplicación Python
-                type: "POST",
-                data: formData,
-                contentType: false,  // No enviar cabeceras de tipo
-                processData: false,  // No procesar los datos
-                success: function(response) {
-                    loading.end();
-                    $('#mensajeFin').html('<b>'+response+'</b>');
-                },
-                error: function(xhr, status, error) {
-                    alert("Error al procesar el archivo: " + error);
+                                // Enviar el archivo a la aplicación Python
+                                $.ajax({
+                                    url: class_admin.cons.send_xml,  // URL de la aplicación Python
+                                    type: "POST",
+                                    data: formData,
+                                    contentType: false,  // No enviar cabeceras de tipo
+                                    processData: false,  // No procesar los datos
+                                    success: function(response) {
+                                        loading.end();
+                                        $('#mensajeFin').html('<b>'+response+'</b>');
+                                    },
+                                    error: function(xhr, status, error) {
+                                        $('#mensajeFin').html('Error al procesar el archivo');
+                                    }
+                                });
+                            }
+                    }
                 }
             });
         });
