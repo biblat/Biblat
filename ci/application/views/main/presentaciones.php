58895a7e03438ee806c91fd162d5c95a96e4a06f
Edgar <eduranm@dgb.unam.mx>
2026-06-25 13:14:52 -0600

    ci/application/views/main/presentaciones.php

--- ci/application/views/main/presentaciones.php
+++ ci/application/views/main/presentaciones.php
@@ -2,6 +2,15 @@
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h5 class="panel-title">
+				<a href="{base_url('archivos/ppt/Conversatorio BIBLAT Dialnet DOAJ Latindex - LatinRev 2026.pptx')}" target="_blank">
+                                    <span class="fa fa-file-pdf-o"></span> Sánchez Pereyra, Antonio (26 de junio de 2026). <b>Los indizadores se presentan y comparan: herramientas y tecnologías para visibilizar la calidad de las revistas.</b> X Jornada LatinRev FLACSO Argentina.
+				</a>
+			</h5>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h5 class="panel-title">
 				<a href="{base_url('archivos/ppt/ODS-Biblat-Aurora - Jornadas LVII 7-may-2026.pptx')}" target="_blank">
                                     <span class="fa fa-file-powerpoint-o"></span> Zetter Patiño, Julio y Zetter Leal, Julio (6-8 de mayo de 2026). <b>De la indización a la inteligencia científica: sistemas bibliográficos, ODS e interoperabilidad semántica en entornos abiertos</b>. LVII Jornadas Mexicanas de Biblioteconomía. Universidad Autónoma de Coahuila.
 				</a>
